25cca910f03ec7ae7416f70bf02d72b41924e787
Singamala Dhanush Reddy <dhanushreddy.1514@gmail.com>
2024-07-07 15:09:49 +0530

    corosol for allcard

--- src/components/AllCard.js
+++ src/components/AllCard.js
@@ -3,14 +3,32 @@ import "./style.scss"
 import Firstcard from './Firstcard'
 import AllCards from './AllCards'
 import { allCardData } from './data'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+
 
 const AllCard = ({ dropDown }) => {
+
+  const settings = {
+    // className: "slider variable-width",
+    dots: false,
+    infinite: false,
+    centerMode: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: false,
+    prevArrow: <h1>⬅️</h1>,
+    nextArrow: <h1>➡️</h1>,
+  };
+
   return (
     <div className='parent'>
+       <Slider {...settings}>
       <Firstcard cname = {'all'} />
       <AllCards dropDown = {dropDown} color = {'green'} allCardData = {allCardData}/>
       <AllCards dropDown = {dropDown} color = {'amber'} allCardData = {allCardData}/>
       <AllCards dropDown = {dropDown} color = {'red'} allCardData = {allCardData}/>
+      </Slider>
     </div>
   )
 }
